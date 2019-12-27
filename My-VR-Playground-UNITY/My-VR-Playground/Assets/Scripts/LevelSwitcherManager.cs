c680e2796f49ebf5a40c8984a24b5a2799f99f9a
Roei Rubach <rrubach@gmail.com>
2019-12-27 21:21:23 +0200

    Added a main menu scene.
    Update DistanceGrab_Playground.
    The main menu can now switch between the different levels by hooping the ball into one of the trashcans.

---
+++ My-VR-Playground-UNITY/My-VR-Playground/Assets/Scripts/LevelSwitcherManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitcherManager : MonoBehaviour
+{
+    [SerializeField]
+    private int levelToSwitchTo;
+
+    [SerializeField]
+    private Canvas blackscreenCanvas;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (levelToSwitchTo)
+        {
+            case 1:
+                print("Entering Grab level");
+                LevelTriggerManager(other);
+                break;
+
+            case 2:
+                print("Entering DistanceGrab level");
+                LevelTriggerManager(other);
+                break;
+
+            case 3:
+                print("Entering Gun level");
+                LevelTriggerManager(other);
+                break;
+
+            case 4:
+                print("Entering Ragdoll level");
+                LevelTriggerManager(other);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void LevelTriggerManager(Collider other)
+    {
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        blackscreenCanvas.transform.GetChild(0).gameObject.SetActive(true);
+        Invoke("SwitchToLevel", 1.2f);
+    }
+
+    private void SwitchToLevel()
+    {
+        SceneManager.LoadScene(levelToSwitchTo);
+    }
+}
