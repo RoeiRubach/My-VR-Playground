78e4ab6f3142582b35df8b8b5bd8a40adae0cdcf
Roei Rubach <rrubach@gmail.com>
2020-01-20 23:49:41 +0200

    List of added objects/items/etc. in the description.
    Reorganized scripts folder for each scene.
    Added physics material to the ball.
    Added sound UX for not reaching the bell with the ball.
    Added a script to the fulcrum, for returning to its origin rotation after moving.

---
+++ My-VR-Playground-UNITY/My-VR-Playground/Assets/Scripts/Ragdoll scene/BallCollisionManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class BallCollisionManager : MonoBehaviour
+{
+    private string _ballUpperLimit = "Ball upper limit";
+    private AudioSource _audioSourceBell;
+    [SerializeField] private AudioSource _maleLaughter;
+    private Animator _dummyAnimator;
+    private Rigidbody _rigidbody;
+    private bool _isBallMoved, _isBallTouchedBell;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _dummyAnimator = _maleLaughter.GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isBallMoved)
+            _isBallMoved = _rigidbody.velocity != Vector3.zero ? true : false;
+
+        else if(_isBallMoved && !_isBallTouchedBell)
+            if (_rigidbody.velocity == Vector3.zero)
+            {
+                _isBallMoved = false;
+                _dummyAnimator.SetBool("didntBallReachBell", true);
+                _maleLaughter.Play();
+                Invoke("SetAnimatorLaughterBoolFalse", 0.2f);
+            }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name == _ballUpperLimit)
+        {
+            _isBallTouchedBell = true;
+            _audioSourceBell = other.transform.GetComponent<AudioSource>();
+            _audioSourceBell.Play();
+            RagdollManager.Instance.SetRagdollON();
+        }
+    }
+
+    private void SetAnimatorLaughterBoolFalse()
+    {
+        _dummyAnimator.SetBool("didntBallReachBell", false);
+    }
+}
---
+++ My-VR-Playground-UNITY/My-VR-Playground/Assets/Scripts/Ragdoll scene/FulcrumController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FulcrumController : MonoBehaviour
+{
+    private Quaternion _startingRotation;
+    private bool _isFulcrumMoved;
+
+    private void Start()
+    {
+        _startingRotation = transform.rotation;
+    }
+
+    private void LateUpdate()
+    {
+        if (transform.rotation != _startingRotation && !_isFulcrumMoved)
+            StartCoroutine(SetFulcrumBack());
+    }
+
+    private IEnumerator SetFulcrumBack()
+    {
+        print("IEnumerator started");
+        _isFulcrumMoved = true;
+        yield return new WaitForSeconds(1f);
+        transform.rotation = _startingRotation;
+        _isFulcrumMoved = false;
+    }
+}
