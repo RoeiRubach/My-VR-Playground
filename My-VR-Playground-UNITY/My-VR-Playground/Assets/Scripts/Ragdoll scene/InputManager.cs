73acb927f05fcb389a71e7a35e1c7845954ecf83
RoeiRubach <44708223+RoeiRubach@users.noreply.github.com>
2020-01-22 20:44:01 +0200

    Added an input controll to restart the game via A button.

---
+++ My-VR-Playground-UNITY/My-VR-Playground/Assets/Scripts/Ragdoll scene/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] OVRInput.Button _restartButton;
+
+    private void Update()
+    {
+        if (OVRInput.GetDown(_restartButton))
+        {
+            SceneManager.LoadScene("Ragdoll");
+        }
+    }
+}
