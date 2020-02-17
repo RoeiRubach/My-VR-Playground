a3908bec046062d4d6b120e3ae04b0ff21909c69
RoeiRubach <44708223+RoeiRubach@users.noreply.github.com>
2020-02-17 16:01:08 +0200

    Made the cube to press at the grab level to be random

---
+++ My-VR-Playground-UNITY/My-VR-Playground/Assets/Scripts/Grab scene/GrabLevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GrabLevelManager : MonoBehaviour
+{
+    [SerializeField] private Transform cubesParent;
+
+    private Transform[] cubesArray;
+
+    private void Start()
+    {
+        cubesArray = cubesParent.GetComponentsInChildren<Transform>();
+
+        int randomNum = Random.Range(0, cubesArray.Length + 1);
+
+        cubesArray[randomNum].tag = "Player";
+    }
+}
